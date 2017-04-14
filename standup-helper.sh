585af3b03ea7d2a3d247de2059e761a36cf08c2c
Monica Dinculescu <noms@google.com>
2017-04-14 11:57:06 -0700

    make standup helper care about days

--- standup-helper.sh
+++ standup-helper.sh
@@ -5,9 +5,27 @@ set -e
 # and run git-standup there directly from node (cwd did nothing), so
 # here we are.
 
-# I used `-m 2` because I have code in ~/Code and ~/Code/some-giant-subfolder
-# and I want reports from both
+progname=$0
+function usage () {
+   echo "Usage: "
+   echo "  $progname [-d days] [-h] repo1 repo2 etc."
+   echo "  -d \t - Specify the number of days back to include"
+   echo "  -d \t - Display this help screen"
+}
+
+# get the optional days
+days=1
+while getopts "d:h" opt; do
+   case $opt in
+   d ) days=$OPTARG;;
+   h )  usage ;;
+   \?)  usage ;;
+   esac
+done
+shift $(($OPTIND - 1))
+
+# the repo names
 for dir in "$@"
 do
-  (cd $dir; git-standup -d 2)
+  (cd $dir; git-standup -d $days)
 done
