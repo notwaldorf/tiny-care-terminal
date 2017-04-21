196cfe01e9c5bd5d26b8c8f01b3fdf187f75c325
Eli Sadoff <esadoff@ardiangroup.com>
2017-04-20 22:34:52 -0400

    Reformat into ES6 and a more standard style

--- care.js
+++ care.js
@@ -1,180 +1,167 @@
 #!/usr/bin/env node
-var config = require(__dirname + '/config.js');
-var twitterbot = require(__dirname + '/twitterbot.js');
+const config = require(__dirname + '/config.js')
+const twitterbot = require(__dirname + '/twitterbot.js')
 
-var spawn = require( 'child_process' ).spawn;
-var blessed = require('blessed');
-var contrib = require('blessed-contrib');
-var chalk = require('chalk');
-var parrotSay = require('parrotsay-api');
-var weather = require('weather-js');
+const spawn = require('child_process').spawn
+const blessed = require('blessed')
+const contrib = require('blessed-contrib')
+const chalk = require('chalk')
+const parrotSay = require('parrotsay-api')
+const weather = require('weather-js')
 
-var screen = blessed.screen(
-    {fullUnicode: true, // emoji or bust
-     smartCSR: true,
-     autoPadding: true,
-     title: '✨💖 tiny care terminal 💖✨'
-    });
+const screen = blessed.screen(
+  {
+    fullUnicode: true, // emoji or bust
+    smartCSR: true,
+    autoPadding: true,
+    title: '✨💖 tiny care terminal 💖✨'
+  })
 
 // Quit on Escape, q, or Control-C.
-screen.key(['escape', 'q', 'C-c'], function(ch, key) {
-  return process.exit(0);
-});
+screen.key(['escape', 'q', 'C-c'], (ch, key) => process.exit(0))
 
 // Refresh on r, or Control-R.
-screen.key(['r', 'C-r'], function(ch, key) {
-  tick();
-});
+screen.key(['r', 'C-r'], (ch, key) => tick())
 
-var grid = new contrib.grid({rows: 12, cols: 12, screen: screen});
+const grid = new contrib.grid({rows: 12, cols: 12, screen: screen})
 
 // grid.set(row, col, rowSpan, colSpan, obj, opts)
-var weatherBox = grid.set(0, 8, 2, 4, blessed.box, makeScrollBox(' 🌤 '));
-var todayBox = grid.set(0, 0, 6, 6, blessed.box, makeScrollBox(' 📝  Today '));
-var weekBox = grid.set(6, 0, 6, 6, blessed.box, makeScrollBox(' 📝  Week '));
-var commits = grid.set(0, 6, 6, 2, contrib.bar, {label: 'Commits', barWidth: 5, xOffset: 4, maxHeight: 10});
-var parrotBox = grid.set(6, 6, 6, 6, blessed.box, makeScrollBox(''));
+const weatherBox = grid.set(0, 8, 2, 4, blessed.box, makeScrollBox(' 🌤 '))
+const todayBox = grid.set(0, 0, 6, 6, blessed.box, makeScrollBox(' 📝  Today '))
+const weekBox = grid.set(6, 0, 6, 6, blessed.box, makeScrollBox(' 📝  Week '))
+const commits = grid.set(0, 6, 6, 2, contrib.bar, {label: 'Commits', barWidth: 5, xOffset: 4, maxHeight: 10})
+const parrotBox = grid.set(6, 6, 6, 6, blessed.box, makeScrollBox(''))
 
-var tweetBoxes = {}
-tweetBoxes[config.twitter[1]] = grid.set(2, 8, 2, 4, blessed.box, makeBox(' 💖 '));
-tweetBoxes[config.twitter[2]] = grid.set(4, 8, 2, 4, blessed.box, makeBox(' 💬 '));
+const tweetBoxes = {}
+tweetBoxes[config.twitter[1]] = grid.set(2, 8, 2, 4, blessed.box, makeBox(' 💖 '))
+tweetBoxes[config.twitter[2]] = grid.set(4, 8, 2, 4, blessed.box, makeBox(' 💬 '))
 
-tick();
-setInterval(tick, 1000 * 60 * config.updateInterval);
+tick()
+setInterval(tick, 1000 * 60 * config.updateInterval)
 
-function tick() {
-  doTheWeather();
-  doTheTweets();
-  doTheCodes();
+function tick () {
+  doTheWeather()
+  doTheTweets()
+  doTheCodes()
 }
 
-function doTheWeather() {
-  weather.find({search: config.weather, degreeType: config.celsius ? 'C' : 'F'}, function(err, result) {
+function doTheWeather () {
+  weather.find({search: config.weather, degreeType: config.celsius ? 'C' : 'F'}, (err, result) => {
     if (result && result[0] && result[0].current) {
-      var json = result[0];
+      const json = result[0]
       // TODO: add emoji for this thing.
-      var skytext = json.current.skytext.toLowerCase();
-      var currentDay = json.current.day;
-      var degreetype = json.location.degreetype;
-      var forecastString = '';
-      for (var i = 0; i < json.forecast.length; i++) {
-        var forecast = json.forecast[i];
-        if (forecast.day === currentDay) {
-          var skytextforecast = forecast.skytextday.toLowerCase();
-          forecastString = `Today, it will be ${skytextforecast} with the forecasted high of ${forecast.high}°${degreetype} and a low of ${forecast.low}°${degreetype}.`;
-        }
-      }
-      weatherBox.content = `In ${json.location.name} it's ${json.current.temperature}°${degreetype} and ${skytext} right now. ${forecastString}`;
+      const skytext = json.current.skytext.toLowerCase()
+      const currentDay = json.current.day
+      const skytextForecast = json.find((forceast) => forecast.day === currentDay).skytextday.toLowerCase()
+      const forecastString = `Today, it will be ${skytextforecast} with the forecasted high of ${forecast.high}°${degreetype} and a low of ${forecast.low}°${degreetype}.`
+      const degreetype = json.location.degreetype
+      weatherBox.content = `In ${json.location.name} it's ${json.current.temperature}°${degreetype} and ${skytext} right now. ${forecastString}`
     } else {
-      weatherBox.content = 'Having trouble fetching the weather for you :(';
+      weatherBox.content = 'Having trouble fetching the weather for you :('
     }
-  });
+  })
 }
 
-function doTheTweets() {
-  for (var which in config.twitter) {
+function doTheTweets () {
+  for (const which in config.twitter) {
     // Gigantor hack: first twitter account gets spoken by the party parrot.
     if (which == 0) {
-      twitterbot.getTweet(config.twitter[which]).then(function(tweet) {
-        parrotSay(tweet.text).then(function(text) {
-          parrotBox.content = text;
-          screen.render();
-        });
-      },function(error) {
+      twitterbot.getTweet(config.twitter[which]).then((tweet) => {
+        parrotSay(tweet.text).then(function (text) {
+          parrotBox.content = text
+          screen.render()
+        })
+      }, function (error) {
         // Just in case we don't have tweets.
-        parrotSay('Hi! You\'re doing great!!!').then(function(text) {
-          parrotBox.content = text;
-          screen.render();
-        });
-      });
+        parrotSay('Hi! You\'re doing great!!!').then((text) => {
+          parrotBox.content = text
+          screen.render()
+        })
+      })
     } else {
-      twitterbot.getTweet(config.twitter[which]).then(function(tweet) {
-        tweetBoxes[tweet.bot.toLowerCase()].content = tweet.text;
-        screen.render();
-      },function(error) {
+      twitterbot.getTweet(config.twitter[which]).then((tweet) => {
+        tweetBoxes[tweet.bot.toLowerCase()].content = tweet.text
+        screen.render()
+      }, (error) => {
         tweetBoxes[config.twitter[1]].content =
-        tweetBoxes[config.twitter[2]].content =
-        'Can\'t read Twitter without some API keys  🐰. Maybe try the scraping version instead?';
-      });
+          tweetBoxes[config.twitter[2]].content =
+            'Can\'t read Twitter without some API keys  🐰. Maybe try the scraping version instead?'
+      })
     }
   }
 }
 
-function doTheCodes() {
-  var todayCommits = 0;
-  var weekCommits = 0;
+function doTheCodes () {
+  let todayCommits = 0
+  let weekCommits = 0
 
-  var today = spawn('sh ' + __dirname + '/standup-helper.sh', [config.repos], {shell:true});
-  todayBox.content = '';
+  const today = spawn('sh ' + __dirname + '/standup-helper.sh', [config.repos], {shell: true})
+  todayBox.content = ''
   today.stdout.on('data', data => {
-    todayCommits = getCommits(`${data}`, todayBox);
-    updateCommitsGraph(todayCommits, weekCommits);
-    screen.render();
-  });
+    todayCommits = getCommits(`${data}`, todayBox)
+    updateCommitsGraph(todayCommits, weekCommits)
+    screen.render()
+  })
 
-  var week = spawn('sh ' + __dirname + '/standup-helper.sh', ['-d 7', config.repos], {shell:true});
-  weekBox.content = '';
+  const week = spawn('sh ' + __dirname + '/standup-helper.sh', ['-d 7', config.repos], {shell: true})
+  weekBox.content = ''
   week.stdout.on('data', data => {
-    weekCommits = getCommits(`${data}`, weekBox);
-    updateCommitsGraph(todayCommits, weekCommits);
-    screen.render();
-  });
+    weekCommits = getCommits(`${data}`, weekBox)
+    updateCommitsGraph(todayCommits, weekCommits)
+    screen.render()
+  })
 }
 
-function makeBox(label) {
+function makeBox (label) {
   return {
     label: label,
     tags: true,
-    // draggable: true,
     border: {
       type: 'line'  // or bg
     },
     style: {
       fg: 'white',
-      border: { fg: 'cyan' },
-      hover: { border: { fg: 'green' }, }
+      border: {fg: 'cyan'},
+      hover: {border: {fg: 'green'},}
     }
-  };
+  }
 }
 
-function makeScrollBox(label) {
-  var options = makeBox(label);
-  options.scrollable = true;
-  options.scrollbar = { ch:' ' };
-  options.style.scrollbar = { bg: 'green', fg: 'white' }
-  options.keys = true;
-  options.vi = true;
-  options.alwaysScroll = true;
-  options.mouse = true;
-  return options;
+function makeScrollBox (label) {
+  const options = makeBox(label)
+  options.scrollable = true
+  options.scrollbar = {ch: ' '}
+  options.style.scrollbar = {bg: 'green', fg: 'white'}
+  options.keys = true
+  options.vi = true
+  options.alwaysScroll = true
+  options.mouse = true
+  return options
 }
 
-var commitRegex = /(.......) (- .*)/g;
-function getCommits(data, box) {
-  var content = colorizeLog(data);
-  box.content += content;
-  return (box.content.match(commitRegex) || []).length;
+const commitRegex = /(.......) (- .*)/g
+function getCommits (data, box) {
+  box.content += colorizeLog(data)
+  return (box.content.match(commitRegex) || []).length
 }
 
-function updateCommitsGraph(today, week) {
+function updateCommitsGraph (today, week) {
   commits.setData({titles: ['today', 'week'], data: [today, week]})
 }
 
-function colorizeLog(text) {
-  var lines = text.split('\n');
-  var regex = /(.......) (- .*) (\(.*\)) (<.*>)/i;
-  for (var i = 0; i < lines.length; i++) {
+function colorizeLog (text) {
+  const lines = text.split('\n')
+  const regex = /(.......) (- .*) (\(.*\)) (<.*>)/i
+  return lines.map((line) => {
     // If it's a path
-    if (lines[i][0] === '/' || lines[i][0] === '\\') {
-      lines[i] = '\n' + chalk.red(lines[i]);
+    if (line[0] === '/' || line[0] === '\\') {
+      return `\n${chalk.red(line)}`
     } else {
       // It's a commit.
-      var matches = lines[i].match(regex);
+      const matches = line.match(regex)
       if (matches) {
-        lines[i] = chalk.red(matches[1]) + ' ' + matches[2] + ' ' +
-            chalk.green(matches[3])
+        return `${chalk.red(matches[1])} ${matches[2]} ${chalk.green(matches[3])}`
       }
     }
-  }
-  return lines.join('\n');
+  }).join('\n')
 }
--- twitterbot.js
+++ twitterbot.js
@@ -1,50 +1,43 @@
-var Twit = require('twit');
-var config = require(__dirname + '/config.js');
-var scraperjs = require('scraperjs');
+const Twit = require('twit')
+const config = require(__dirname + '/config.js')
+const scraperjs = require('scraperjs')
 
-var T = new Twit({
-  consumer_key:        config.keys.consumer_key,
-  consumer_secret:     config.keys.consumer_secret,
-  access_token:        config.keys.access_token,
+const T = new Twit({
+  consumer_key: config.keys.consumer_key,
+  consumer_secret: config.keys.consumer_secret,
+  access_token: config.keys.access_token,
   access_token_secret: config.keys.access_token_secret,
-  timeout_ms:          60*1000,  // optional HTTP request timeout to apply to all requests.
-});
+  timeout_ms: 60 * 1000,  // optional HTTP request timeout to apply to all requests.
+})
 
-var options = {exclude_replies:true, include_rts:false, count: 1 };
+const options = {exclude_replies: true, include_rts: false, count: 1}
 
-function getTweet(who) {
-  who = who || 'tinycarebot';
-  return config.apiKeys ? apiTweet(who) : scrapeTweet(who);
-}
+getTweet = (who = 'tinycarebot') => config.apiKeys ? apiTweet(who) : scrapeTweet(who)
 
-function apiTweet(who) {
-  options.screen_name = who;
-  return new Promise(function (resolve, reject) {
-    T.get('statuses/user_timeline', options, function(err, data) {
+function apiTweet (who) {
+  options.screen_name = who
+  return new Promise((resolve, reject) => {
+    T.get('statuses/user_timeline', options, (err, data) => {
       if (err) {
-        reject('This didn\'t work. Maybe you didn\'t set up the twitter API keys?');
+        reject('This didn\'t work. Maybe you didn\'t set up the twitter API keys?')
       } else {
-        resolve({text:data[0].text, bot: data[0].user.screen_name});
+        resolve({text: data[0].text, bot: data[0].user.screen_name})
       }
-    });
-  });
+    })
+  })
 }
 
-function scrapeTweet(who) {
-  return new Promise(function (resolve, reject) {
+function scrapeTweet (who) {
+  return new Promise((resolve, reject) => {
     scraperjs.StaticScraper.create('https://twitter.com/' + who)
-        .scrape(function($) {
-            return $(".js-tweet-text.tweet-text").map(function() {
-                return $(this).text();
-            }).get();
-        })
-        .then(function(tweets) {
-          var tweetNumber = Math.floor(Math.random() * tweets.length);
-          resolve({text:tweets[tweetNumber], bot: who});
-        },function(error) {
-          reject('Can\'t scrape tweets. Maybe the user is private or doesn\'t exist?');
-        });
-  });
+      .scrape(($) => $('.js-tweet-text.tweet-text').map(() => $(this).text()).get())
+      .then((tweets) => {
+        const tweetNumber = Math.floor(Math.random() * tweets.length)
+        resolve({text: tweets[tweetNumber], bot: who})
+      }, (error) => {
+        reject('Can\'t scrape tweets. Maybe the user is private or doesn\'t exist?')
+      })
+  })
 }
 
-module.exports.getTweet = getTweet;
+module.exports.getTweet = getTweet
