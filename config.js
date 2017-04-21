a2125b0fc6c9ac69355068f77d7cac0b233127b3
Eli Sadoff <esadoff@ardiangroup.com>
2017-04-21 00:14:46 -0400

    Made requested changes

--- config.js
+++ config.js
@@ -10,21 +10,21 @@ const config = {
   // Directories in which to run git-standup on for a list of your recent commits.
   repos: (process.env.TTC_REPOS || '~/Code').replace(/,/g, ' '),
 
-// Where to check the weather for.
-// It's using weather.service.msn.com behind the curtains.
+  // Where to check the weather for.
+  // It's using weather.service.msn.com behind the curtains.
   weather: process.env.TTC_WEATHER || 'San Francisco',
 
   // Set to false if you're an imperial savage. <3
-  celsius: (process.env.TTC_CELSIUS || 'true') === 'true',
+  celsius:        (process.env.TTC_CELSIUS || 'true') === 'true',
   updateInterval: parseFloat(process.env.TTC_UPDATE_INTERVAL) || 20,
   // Set to false if you're an imperial savage. <3
 
   keys: {
-    consumer_key: process.env.TTC_CONSUMER_KEY || process.env.CONSUMER_KEY || 'none',
-    consumer_secret: process.env.TTC_CONSUMER_SECRET || process.env.CONSUMER_SECRET || 'none',
-    access_token: process.env.TTC_ACCESS_TOKEN || process.env.ACCESS_TOKEN || 'none',
+    consumer_key:        process.env.TTC_CONSUMER_KEY || process.env.CONSUMER_KEY || 'none',
+    consumer_secret:     process.env.TTC_CONSUMER_SECRET || process.env.CONSUMER_SECRET || 'none',
+    access_token:        process.env.TTC_ACCESS_TOKEN || process.env.ACCESS_TOKEN || 'none',
     access_token_secret: process.env.TTC_ACCESS_TOKEN_SECRET || process.env.ACCESS_TOKEN_SECRET || 'none',
   }
-}
+};
 
-module.exports = config
+module.exports = config;
--- twitterbot.js
+++ twitterbot.js
@@ -1,43 +1,43 @@
-const Twit = require('twit')
-const config = require(__dirname + '/config.js')
-const scraperjs = require('scraperjs')
+const Twit      = require('twit');
+const config    = require(__dirname + '/config.js');
+const scraperjs = require('scraperjs');
 
 const T = new Twit({
-  consumer_key: config.keys.consumer_key,
-  consumer_secret: config.keys.consumer_secret,
-  access_token: config.keys.access_token,
+  consumer_key:        config.keys.consumer_key,
+  consumer_secret:     config.keys.consumer_secret,
+  access_token:        config.keys.access_token,
   access_token_secret: config.keys.access_token_secret,
-  timeout_ms: 60 * 1000,  // optional HTTP request timeout to apply to all requests.
-})
+  timeout_ms:          60 * 1000,  // optional HTTP request timeout to apply to all requests.
+});
 
-const options = {exclude_replies: true, include_rts: false, count: 1}
+const options = {exclude_replies: true, include_rts: false, count: 1};
 
-getTweet = (who = 'tinycarebot') => config.apiKeys ? apiTweet(who) : scrapeTweet(who)
+getTweet = (who = 'tinycarebot') => config.apiKeys ? apiTweet(who) : scrapeTweet(who);
 
-function apiTweet (who) {
-  options.screen_name = who
+function apiTweet(who) {
+  options.screen_name = who;
   return new Promise((resolve, reject) => {
     T.get('statuses/user_timeline', options, (err, data) => {
       if (err) {
-        reject('This didn\'t work. Maybe you didn\'t set up the twitter API keys?')
+        reject('This didn\'t work. Maybe you didn\'t set up the twitter API keys?');
       } else {
-        resolve({text: data[0].text, bot: data[0].user.screen_name})
+        resolve({text: data[0].text, bot: data[0].user.screen_name});
       }
-    })
-  })
+    });
+  });
 }
 
-function scrapeTweet (who) {
+function scrapeTweet(who) {
   return new Promise((resolve, reject) => {
     scraperjs.StaticScraper.create('https://twitter.com/' + who)
       .scrape(($) => $('.js-tweet-text.tweet-text').map(() => $(this).text()).get())
       .then((tweets) => {
-        const tweetNumber = Math.floor(Math.random() * tweets.length)
-        resolve({text: tweets[tweetNumber], bot: who})
+        const tweetNumber = Math.floor(Math.random() * tweets.length);
+        resolve({text: tweets[tweetNumber], bot: who});
       }, (error) => {
-        reject('Can\'t scrape tweets. Maybe the user is private or doesn\'t exist?')
-      })
-  })
+        reject('Can\'t scrape tweets. Maybe the user is private or doesn\'t exist?');
+      });
+  });
 }
 
-module.exports.getTweet = getTweet
+module.exports.getTweet = getTweet;
