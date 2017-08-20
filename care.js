d9b2655afacfb9d5dc64e79d374cbc0ff6415187
Gaurav Chikhale <gauravchl@users.noreply.github.com>
2017-08-20 14:50:16 +0530

    Show pomodoro counts

--- care.js
+++ care.js
@@ -341,8 +341,23 @@ var pomodoroHandlers = {
     }
 
     var content = `In Pomodoro Mode: ${remainingTime} ${statusText}`;
-    var metaData = `Duration: ${pomodoroObject.getRunningDuration()} Minutes,  Break Time: ${pomodoroObject.getBreakDuration()} Minutes\n`;
-    metaData += 'commands: \n s - start/pause/resume \n e - stop \n u - update duration \n b - update break time';
+
+    var metaData = `
+      Duration: ${pomodoroObject.getRunningDuration()}m, Break Time: ${pomodoroObject.getBreakDuration()}m,
+
+      Pomodoro Stats:
+        Total run today: ${pomodoroObject.getTotalPomodoroCount('day')}
+        Total run this Week: ${pomodoroObject.getTotalPomodoroCount('week')}
+        Total run this Month: ${pomodoroObject.getTotalPomodoroCount('month')}
+
+
+      commands:
+        s - start/pause/resume
+        e - stop
+        u - update duration
+        b - update break time
+    `;
+    metaData.replace(/(\r?\n|\r)\s+/gm, '\n');
     parrotBox.content = getAnsiArt(content) + metaData;
     screen.render();
   },
